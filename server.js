1997d62e4ce8a7321fa2b1b85027509557483650
varmav47-dev <varmav47@gmail.com>
2026-01-06 20:01:04 +0530

    Fix user pairing logic

--- server.js
+++ server.js
@@ -14,37 +14,36 @@ io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   if (waitingUser) {
-    // pair found
-    const room = `room-${waitingUser.id}-${socket.id}`;
+    socket.partner = waitingUser;
+    waitingUser.partner = socket;
 
-    socket.join(room);
-    waitingUser.join(room);
-
-    io.to(room).emit("matched");
+    socket.emit("matched");
+    waitingUser.emit("matched");
 
     waitingUser = null;
   } else {
     waitingUser = socket;
+    socket.emit("waiting");
   }
 
   socket.on("message", (msg) => {
-    const rooms = [...socket.rooms];
-    rooms.forEach((room) => {
-      if (room !== socket.id) {
-        socket.to(room).emit("message", msg);
-      }
-    });
+    if (socket.partner) {
+      socket.partner.emit("message", msg);
+    }
   });
 
   socket.on("disconnect", () => {
-    if (waitingUser?.id === socket.id) {
+    if (socket.partner) {
+      socket.partner.emit("partner-disconnected");
+      socket.partner.partner = null;
+    }
+    if (waitingUser === socket) {
       waitingUser = null;
     }
-    console.log("User disconnected:", socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () =>
-  console.log("Server running on port", PORT)
-);
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
